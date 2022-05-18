8a2a4f85cb5393a83d8a536090ba32f2a2a264d9
VeronicaTaucci <taucci.veronica@gmail.com>
2022-05-18 11:49:39 -0400

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,7 @@ import SignIn from "./components/auth/Signin";
 
 function App() {
   return (
-    <Router>
-      <BaseLayout>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/albums" element={<Albums />} />
-          <Route path="/album/:albumID" element={<Album />} />
-        </Routes>
-      </BaseLayout>
-    </Router>
+    <SignIn/>
   )
 }
 
