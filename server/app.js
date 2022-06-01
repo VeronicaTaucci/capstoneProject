0e0d53071b2cf09cbdc5ccc4027e780817cd7204
VeronicaTaucci <taucci.veronica@gmail.com>
2022-06-01 14:15:50 -0400

    heroku set up

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 
 require('dotenv').config()
-
+const path = require('path')
 const port = process.env.PORT || 3001;
 
+app.use(express.static(path.join(__dirname, '/public')))
 app.use(express.urlencoded({ extended: false })) // scrape email and pwd from request header
+
 app.use(express.json())  //req.body
 
 app.use(require('./routes/authentication'))
@@ -13,7 +15,9 @@ app.use(require('./routes/authentication'))
 app.get('/', (req, res) => {
     res.send("Hello World")
 })
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'))
+})
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
